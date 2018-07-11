a4e44f5308cdd9b2b995e13e0b7902f802499aa6
Casbi <yuanyue673@gmail.com>
2018-07-11 23:04:28 +1200

    still working out the departure and arrival time

--- tinyServer.go
+++ tinyServer.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"golang.org/x/net/context"
 	"googlemaps.github.io/maps"
 )
 
+type trip struct {
+	departureTime          time.Time
+	arrivalTime            time.Time
+	totalDurationInTraffic time.Duration
+	totalDuration          time.Duration
+}
+
 func timeToGoHandler(w http.ResponseWriter, req *http.Request) {
+	var thisTrip trip
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("testing - writen by TimeToGoHandler\n"))
@@ -29,19 +39,41 @@ func timeToGoHandler(w http.ResponseWriter, req *http.Request) {
 		Mode:          maps.TravelModeDriving,
 	}
 
+	departureTime, err := strconv.ParseInt(dirReq.DepartureTime, 10, 64)
+	if err != nil {
+		log.Printf("error parsing departure time from string to int64: %s", err)
+	}
+
+	thisTrip.departureTime = time.Unix(0, departureTime)
+	fmt.Println("Trip departureTime: ", thisTrip.departureTime)
+
 	mapsClient, err := maps.NewClient(maps.WithAPIKey("AIzaSyAFgh1pAQpS59mEwuViE2ExOw7M_W-2rzQ"))
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
 	}
 
 	routes, _, err := mapsClient.Directions(context.Background(), dirReq)
+
 	if err != nil {
 		log.Printf("fatal error: %s", err)
 	} else {
 		for i := range routes {
 			fmt.Println(routes[i].Summary)
 			for j := range routes[i].Legs {
-				fmt.Println(routes[i].Legs[j].DurationInTraffic)
+				if j == len(routes[i].Legs) {
+					thisTrip.arrivalTime = routes[i].Legs[j].ArrivalTime
+					fmt.Println("Trip departureTime: ", thisTrip.arrivalTime)
+				}
+
+				thisTrip.totalDurationInTraffic += routes[i].Legs[j].DurationInTraffic
+				thisTrip.totalDuration += routes[i].Legs[j].Duration
+
+				fmt.Println("Leg DurationInTraffic: ", routes[i].Legs[j].DurationInTraffic)
+				fmt.Println("Leg Duration: ", routes[i].Legs[j].Duration)
+
+				fmt.Println("Trip totalDurationInTraffic: ", thisTrip.totalDurationInTraffic)
+				fmt.Println("Trip totalDuration : ", thisTrip.totalDuration)
+
 			}
 		}
 	}
