54fac5ae8a1f49e702c017a76ed6215a6d739c4e
Yuan <yuan@Yuans-MacBook-Pro.local>
2018-07-13 17:02:46 +1200

    added arrival time print out routes json in the response writer

--- tinyServer.go
+++ tinyServer.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"encoding/json"
 
 	"golang.org/x/net/context"
 	"googlemaps.github.io/maps"
@@ -22,7 +23,7 @@ func timeToGoHandler(w http.ResponseWriter, req *http.Request) {
 	var thisTrip trip
 
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("testing - writen by TimeToGoHandler\n"))
+	fmt.Fprintln(w, "testing - writen by TimeToGoHandler")
 
 	log.Println(req.RequestURI)
 
@@ -30,22 +31,31 @@ func timeToGoHandler(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Printf("error parsing form: %s", err)
 	}
-	log.Printf("from = %s & to = %s & departure time = %s", req.Form.Get("from"), req.Form.Get("to"), req.Form.Get("departureTime"))
+	fmt.Fprintln(w, "from: " + req.Form.Get("from") + " to: " + req.Form.Get("to") + " departure time: " + req.Form.Get("departureTime"))
 
 	dirReq := &maps.DirectionsRequest{
 		Origin:        req.Form.Get("from"),
 		Destination:   req.Form.Get("to"),
 		DepartureTime: req.Form.Get("departureTime"),
+		ArrivalTime: 	 req.Form.Get("arrivalTime"),
 		Mode:          maps.TravelModeDriving,
 	}
 
 	departureTime, err := strconv.ParseInt(dirReq.DepartureTime, 10, 64)
 	if err != nil {
 		log.Printf("error parsing departure time from string to int64: %s", err)
+	} else {
+		thisTrip.departureTime = time.Unix(departureTime, 0)
+		fmt.Fprintf(w, "Trip departureTime: %v\n", thisTrip.departureTime)
 	}
 
-	thisTrip.departureTime = time.Unix(0, departureTime)
-	fmt.Println("Trip departureTime: ", thisTrip.departureTime)
+	arrivalTime, err := strconv.ParseInt(dirReq.ArrivalTime, 10, 64)
+	if err != nil {
+		log.Printf("error parsing arrival time from string to int64: %s", err)
+	} else {
+		thisTrip.arrivalTime = time.Unix(arrivalTime, 0)
+		fmt.Fprintf(w, "Trip arrivalTime: %v\n", thisTrip.arrivalTime)
+	}
 
 	mapsClient, err := maps.NewClient(maps.WithAPIKey("AIzaSyAFgh1pAQpS59mEwuViE2ExOw7M_W-2rzQ"))
 	if err != nil {
@@ -58,25 +68,28 @@ func timeToGoHandler(w http.ResponseWriter, req *http.Request) {
 		log.Printf("fatal error: %s", err)
 	} else {
 		for i := range routes {
-			fmt.Println(routes[i].Summary)
+			fmt.Println(w, routes[i].Summary)
 			for j := range routes[i].Legs {
-				if j == len(routes[i].Legs) {
+				if j == len(routes[i].Legs)-1 {
 					thisTrip.arrivalTime = routes[i].Legs[j].ArrivalTime
-					fmt.Println("Trip departureTime: ", thisTrip.arrivalTime)
+					fmt.Fprintf(w, "ArrivalTime on last leg: %v\n", routes[i].Legs[j].ArrivalTime)
+					fmt.Fprintf(w, "Trip arrivalTime: %v\n", thisTrip.arrivalTime)
 				}
-
 				thisTrip.totalDurationInTraffic += routes[i].Legs[j].DurationInTraffic
 				thisTrip.totalDuration += routes[i].Legs[j].Duration
 
-				fmt.Println("Leg DurationInTraffic: ", routes[i].Legs[j].DurationInTraffic)
-				fmt.Println("Leg Duration: ", routes[i].Legs[j].Duration)
+				fmt.Fprintf(w, "Leg DurationInTraffic: %v\n", routes[i].Legs[j].DurationInTraffic)
+				fmt.Fprintf(w, "Leg Duration: %v\n", routes[i].Legs[j].Duration)
 
-				fmt.Println("Trip totalDurationInTraffic: ", thisTrip.totalDurationInTraffic)
-				fmt.Println("Trip totalDuration : ", thisTrip.totalDuration)
+				fmt.Fprintf(w, "Trip totalDurationInTraffic: %v\n", thisTrip.totalDurationInTraffic)
+				fmt.Fprintf(w, "Trip totalDuration: %v\n", thisTrip.totalDuration)
 
 			}
 		}
 	}
+
+	routesJSON, _ := json.Marshal(routes)
+	w.Write([]byte(routesJSON))
 
 }
 
